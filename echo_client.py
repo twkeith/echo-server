6339cf80a7500a13ab9035e3777c14787c91c3f8
twkeith <61339265+twkeith@users.noreply.github.com>
2020-10-04 11:17:17 -0700

    My server works, i think

--- echo_client.py
+++ echo_client.py
@@ -7,19 +7,22 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
     # TODO: connect your socket to the server here.
+    sock.connect(server_address)
 
     # you can use this variable to accumulate the entire message received back
     # from the server
-    received_message = ''
+    received_message = b''
 
     # this try/finally block exists purely to allow us to close the socket
     # when we are finished with it
     try:
         print('sending "{0}"'.format(msg), file=log_buffer)
         # TODO: send your message to the server here.
+
+        sock.sendall(msg.encode('utf-8'))
 
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks. Accumulate the chunks you get to build the
@@ -28,19 +31,25 @@ def client(msg, log_buffer=sys.stderr):
         #
         #       Log each chunk you receive.  Use the print statement below to
         #       do it. This will help in debugging problems
-        chunk = ''
-        print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+        while True:
+            chunk = sock.recv(16)
+            received_message += chunk
+            print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+            if len(chunk) < 16:
+                break
     except Exception as e:
         traceback.print_exc()
         sys.exit(1)
     finally:
         # TODO: after you break out of the loop receiving echoed chunks from
         #       the server you will want to close your client socket.
+        sock.close()
         print('closing socket', file=log_buffer)
 
         # TODO: when all is said and done, you should return the entire reply
-        # you received from the server as the return value of this function.
-
+        #
+        print(received_message.decode('utf8'))
+        return received_message.decode('utf8')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -50,3 +59,4 @@ if __name__ == '__main__':
 
     msg = sys.argv[1]
     client(msg)
+    # cd C:\PythonCert\FallClass\Activity02\echo-server
